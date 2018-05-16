fe7398d35d68b1e62288b35be5ff1fd66e40c154
hminh1208 <hmpha7@student.monash.edu>
2018-05-16 18:21:22 +1000

    *Update layout

--- src/monash/pinwheel/dao/SportFacilityDbUtil.java
+++ src/monash/pinwheel/dao/SportFacilityDbUtil.java
@@ -4,6 +4,7 @@ import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
